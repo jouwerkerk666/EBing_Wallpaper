4051bb7bc94c3995ea3ce7079d3e6de6c4d03fec
John Ouwerkerk <jouwerkerk@lsrv1.com>
2022-10-23 22:55:29 +0200

    Final version 1

--- EBing_Wallpaper.go
+++ EBing_Wallpaper.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"bytes"
 	"crypto/tls"
 	"fmt"
 	"image"
@@ -17,15 +18,16 @@ import (
 )
 
 var (
-	Help, Verbose, Version, Noop bool = false, false, false, false
-	Last                         bool = false
-	Past                         int  = 1
-	BingWallPaperUrl             string
-	Title, Copyright, StartDate  string
+	Help, Verbose, Version, Noop, Keep bool = false, false, false, false, false
+	Past                               int  = 0
+	IDX                                string
+	BingWallPaperUrl                   string
+	Title, Copyright, StartDate        string
 )
 
 const (
-	EBingVersion = "0.0.1-beta"
+	EBingVersion = "1.0"
+	ShellToUse   = "bash"
 )
 
 func UNUSED(x ...interface{}) {}
@@ -34,7 +36,6 @@ func GetBingWallpaper(url string) []byte {
 	customTransport := http.DefaultTransport.(*http.Transport).Clone()
 	customTransport.TLSClientConfig = &tls.Config{InsecureSkipVerify: true}
 	client := &http.Client{Transport: customTransport}
-	//os.Setenv("HTTPS_PROXY", "http://host.docker.internal:3128")
 
 	res, err := client.Get(url)
 	if err != nil {
@@ -44,29 +45,7 @@ func GetBingWallpaper(url string) []byte {
 	if err != nil {
 		fmt.Println(err.Error())
 	}
-
 	return body
-}
-
-func init() {
-	getopt.FlagLong(&Help, "help", 'h', "Display help")
-	getopt.FlagLong(&Version, "version", 'V', "Display version")
-	getopt.FlagLong(&Verbose, "verbose", 'v', "Verbose output")
-	getopt.FlagLong(&Noop, "noop", 'n', "Do not Execute. Only shows what will be done.")
-	getopt.FlagLong(&Last, "last", 'l', "Get the latest wallpaper from Bing.")
-	getopt.FlagLong(&Past, "past", 'p', "is the previous [n] wallpaper from Bing.")
-	err := getopt.Getopt(nil)
-	if err != nil || Help {
-		if !Help {
-			fmt.Fprintln(os.Stderr, err)
-		}
-		getopt.Usage()
-		os.Exit(1)
-	}
-	if Version {
-		fmt.Printf("Version: %v\n", EBingVersion)
-		os.Exit(1)
-	}
 }
 
 func GetBingPicture(srcUrl string, destFile string) (int, int) {
@@ -103,42 +82,64 @@ func GetBingPicture(srcUrl string, destFile string) (int, int) {
 		fmt.Println("Error Opening file ", err)
 	}
 	if Verbose {
-		fmt.Printf("DEBUG: Downloaded a file %s with size %d, (%v:%v)\n\n", fileName, size, Xsize, Ysize)
+		fmt.Printf("Downloaded a file %s with size %d Geometry:(%v:%v)\n\n", fileName, size, Xsize, Ysize)
 	}
 	return Xsize, Ysize
+}
 
+func Shellout(command string) (string, string, error) {
+	var stdout bytes.Buffer
+	var stderr bytes.Buffer
+	cmd := exec.Command(ShellToUse, "-c", command)
+	cmd.Stdout = &stdout
+	cmd.Stderr = &stderr
+	err := cmd.Run()
+	return stdout.String(), stderr.String(), err
+}
+
+func init() {
+	getopt.FlagLong(&Help, "help", 'h', "Display help")
+	getopt.FlagLong(&Version, "version", 'V', "Display version")
+	getopt.FlagLong(&Verbose, "verbose", 'v', "Verbose output")
+	getopt.FlagLong(&Noop, "noop", 'n', "Do not Execute. Only shows what will be done.")
+	getopt.FlagLong(&Keep, "keep", 'k', "Keep the tmp files, do not discard them.")
+	getopt.FlagLong(&Past, "past", 'p', "is the previous [n] wallpaper from Bing. Max 7 days")
+	getopt.Parse()
+	err := getopt.Getopt(nil)
+	if err != nil || Help {
+		if !Help {
+			fmt.Fprintln(os.Stderr, err)
+		}
+		getopt.Usage()
+		os.Exit(1)
+	}
+	if Past > 7 {
+		fmt.Println("The max for past is 7.")
+		Past = 7
+	}
+
+	if Version {
+		fmt.Printf("Version: %v\n", EBingVersion)
+		os.Exit(1)
+	}
 }
 
 func main() {
 	var (
-		aspect float32
+		aspect      float32
+		out, errout string
+		cmderr      error
 	)
+
 	DeskTop := string(os.Getenv("DESKTOP"))
-	if DeskTop != "ENLIGHTENMENT" && !Noop {
+	if DeskTop != "Enlightenment" && !Noop {
 		fmt.Printf("Your desktop is not Enlightenment, so this is not working.\n")
 		os.Exit(1)
 	}
 	fmt.Printf("Change wallpaper in Enlightenment\n")
-	BingJSON := GetBingWallpaper("https://www.bing.com/HPImageArchive.aspx?format=js&idx=0&n=1&mkt=en-US")
-	BingWallPaperJSON, _ := jason.NewObjectFromBytes(BingJSON)
-	Images, _ := BingWallPaperJSON.GetObjectArray("images")
-	for _, Image := range Images {
-		WallPaperUrl, _ := Image.GetString("url")
-		WPStartDate, _ := Image.GetString("startdate")
-		WPTitle, _ := Image.GetString("title")
-		WPCopyright, _ := Image.GetString("copyright")
 
-		BingWallPaperUrl = fmt.Sprintf("https://www.bing.com%v", WallPaperUrl)
-		StartDate = fmt.Sprintf("%v", WPStartDate)
-		//Title = fmt.Sprintf("%v", WPTitle)
-		Title = strings.ReplaceAll(WPTitle, " ", "_")
-		Copyright = fmt.Sprintf("%v", WPCopyright)
-	}
-	TempDir := fmt.Sprintf("/tmp/%v/BingWallPaper", os.Getenv("USER"))
 	DestDir := fmt.Sprintf("%v/.e/e/backgrounds", os.Getenv("HOME"))
-	ImageFile := fmt.Sprintf("%v/%v-%v.jpg", TempDir, StartDate, Title)
-	EdjeFile := strings.ReplaceAll(ImageFile, ".jpg", ".edc")
-
+	TempDir := fmt.Sprintf("%v/Pictures/BingWallpapers", os.Getenv("HOME"))
 	// Creating the directories
 	err := os.MkdirAll(DestDir, os.ModePerm)
 	if err != nil {
@@ -149,6 +150,24 @@ func main() {
 		log.Println(err)
 	}
 
+	IDX = getopt.GetValue("past")
+	BingUrl := fmt.Sprintf("https://www.bing.com/HPImageArchive.aspx?format=js&idx=%v&n=1&mkt=en-US", IDX)
+	BingJSON := GetBingWallpaper(BingUrl)
+	BingWallPaperJSON, _ := jason.NewObjectFromBytes(BingJSON)
+	Images, _ := BingWallPaperJSON.GetObjectArray("images")
+	for _, Image := range Images {
+		WallPaperUrl, _ := Image.GetString("url")
+		WPStartDate, _ := Image.GetString("startdate")
+		WPTitle, _ := Image.GetString("title")
+		WPCopyright, _ := Image.GetString("copyright")
+		BingWallPaperUrl = fmt.Sprintf("https://www.bing.com%v", WallPaperUrl)
+		StartDate = fmt.Sprintf("%v", WPStartDate)
+		Title = strings.ReplaceAll(WPTitle, " ", "_")
+		Copyright = fmt.Sprintf("%v", WPCopyright)
+	}
+	ImageFile := fmt.Sprintf("%v/%v-%v.jpg", TempDir, StartDate, Title)
+	EdjeFile := fmt.Sprintf("%v/bing_wallpaper_%v.edc", DestDir, StartDate)
+
 	Xsize, Ysize := GetBingPicture(BingWallPaperUrl, ImageFile)
 	if (Xsize != 0) && (Ysize != 0) {
 		aspect = float32(Xsize) / float32(Ysize)
@@ -156,29 +175,32 @@ func main() {
 		aspect = 0.0
 	}
 
-	EFLTemplate := fmt.Sprintf("images { image: \"%v\" USER; }\n	collections {\n	  group {\n	  name: \"e/desktop/background\";\n 	  data { item: \"style\" \"4\"; item: \"noanimation\" \"1\"; }\n 	  max: %v %v;\n 	  parts {\n 		part {\n 		name: \"bg\";\n 		mouse_events: 0;\n		description {\n		  state: \"default\" 0.0;\n		  aspect: %.1f %.1f;\n		  aspect_preference: NONE;\n		  image { normal: \"%v\"; scale_hint: STATIC; }\n 		}\n		}\n	  }\n	  }\n	}\n	", ImageFile, Xsize, Ysize, aspect, aspect, ImageFile)
-	//	edje_cc_filename = fmt.Sprintf("/tmp/%v/.edc"
+	EFLTemplate := fmt.Sprintf("\nimages { image: \"%v\" USER; }\ncollections {\n  group {\n  name: \"e/desktop/background\";\n  data { item: \"style\" \"4\"; item: \"noanimation\" \"1\"; }\n  max: %v %v;\n  parts {\n    part {\n    name: \"bg\";\n    mouse_events: 0;\n    description {\n      state: \"default\" 0.0;\n      aspect: %.9f %.9f;\n      aspect_preference: NONE;\n      image { normal: \"%v\"; scale_hint: STATIC; }\n    }\n    }\n  }\n  }\n}\n", ImageFile, Xsize, Ysize, aspect, aspect, ImageFile)
 	err = os.WriteFile(EdjeFile, []byte(EFLTemplate), 0755)
 	if err != nil {
 		fmt.Printf("Unable to open file %v for writing\n", EdjeFile)
 	}
-	BingWallPaperFile := fmt.Sprintf("%v/.e/e/background/bing_wallpaper_%v", os.Getenv("$HOME"), StartDate)
-	edje_cc := fmt.Sprintf("edje_cc \"%v\" \"%v\"", BingWallPaperFile, EdjeFile)
+	BingWallPaperFile := fmt.Sprintf("%v/bing_wallpaper_%v.edj", DestDir, StartDate)
+	edje_cc := fmt.Sprintf("edje_cc %v %v", EdjeFile, BingWallPaperFile)
 	if !Noop {
-		cmd := exec.Command("/bin/bash", "-c", edje_cc)
-		cmderr := cmd.Run()
+		out, errout, cmderr = Shellout(edje_cc)
 		if cmderr != nil {
 			log.Printf("error: %v\n", cmderr)
 		}
 	}
+
 	if Verbose {
-		fmt.Printf("DEBUG:\n %v \n %v \n %v \n %v \n", BingWallPaperUrl, StartDate, Title, Copyright)
-		fmt.Printf("DEBUG:\n %v\n\n", EFLTemplate)
-		fmt.Printf("DEGUG:\n %v \n", edje_cc)
-		fmt.Printf("DEBUG: Aspect = %.1f\n", aspect)
+		fmt.Println("Verbose output")
+		fmt.Println("--- edje_cc stdout ---")
+		fmt.Println(out)
+		fmt.Println("--- edje_cc stderr ---")
+		fmt.Println(errout)
+		fmt.Printf("%v \n %v \n %v \n", StartDate, Title, Copyright)
+		fmt.Printf("%v \n", edje_cc)
+		fmt.Printf("Aspect = %.1f\n", aspect)
 	}
 	for x := 0; x <= 2; x++ {
-		command := fmt.Sprintf("enlightenment_remote -desktop-bg-add -1 %v -1 -1 %v.edj", x, BingWallPaperFile)
+		command := fmt.Sprintf("enlightenment_remote -desktop-bg-add -1 %v -1 -1 %v", x, BingWallPaperFile)
 		if !Noop {
 			cmd := exec.Command("/bin/bash", "-c", command)
 			cmderr := cmd.Run()
@@ -187,12 +209,15 @@ func main() {
 			}
 		}
 		if Verbose {
-			fmt.Printf("DEBUG: %v\n", command)
+			fmt.Printf("%v\n", command)
 		}
 	}
-	if !Noop {
-		rdir := fmt.Sprintf("/tmp/%v", os.Getenv("$USER"))
-		err := os.RemoveAll(rdir)
+	if !Keep {
+		err := os.Remove(EdjeFile)
+		if err != nil {
+			log.Fatal(err)
+		}
+		err = os.Remove(ImageFile)
 		if err != nil {
 			log.Fatal(err)
 		}
